ac68107b173058727d0050ed91be5f1bdf448306
JEPTAC <j.perez@ei.com.co>
2026-09-14 14:31:39 -0500

    Add Lumina demo: playwright.config.cjs

---
+++ playwright.config.cjs
@@ -0,0 +1,7 @@
+const {defineConfig}=require('@playwright/test');
+module.exports=defineConfig({
+  testDir:'./tests/browser',timeout:90000,workers:1,
+  use:{baseURL:'http://127.0.0.1:8080',viewport:{width:1280,height:900},screenshot:'only-on-failure',trace:'retain-on-failure'},
+  webServer:{command:'npm start',url:'http://127.0.0.1:8080',reuseExistingServer:!process.env.CI},
+  reporter:[['list'],['html',{open:'never'}]]
+});
